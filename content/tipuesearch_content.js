ce27b48a1bb40aad2d3a852ec5b0f54c69575b68
40823117 <40823117@gm.nfu.edu.tw>
2021-02-27 16:46:52 +0800

    w1 vu,6wj/6vu. e93hk4g4

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'This is  https://github.com/mdecourse/cmstemplate 目前的 cmsimde 在編輯器下方新增一個 csave 按鈕, 意即 collabrative save, 當動態網際管理系統在多人同時維護登入維護網頁內容時, 編輯各頁面時段, 該頁面可能已經改版, 因此使用 csave 按鈕存檔時, 會導入當下最新的該頁面內容, 並試圖與編輯中的頁面內容進行合併. 使用 csave 按鈕存檔無法刪除頁面資料. 使用 Edit All 模式, 無法使用 csave 按鈕（尚未測試).', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': '組員', 'text': '40823117 潘奕丞 \n 40823122 吳家凱 \n', 'tags': '', 'url': '組員.html'}, {'title': '偕同', 'text': '', 'tags': '', 'url': '偕同.html'}]};
