733d4e2a808e0623a89021696a8b9b0513ab941e
40823117 <40823117@gm.nfu.edu.tw>
2021-05-06 00:43:28 +0800

    api

--- cms.bat
+++ cms.bat
@@ -1,2 +1,2 @@
 
-python cmsimde/wsgi.py 1> nul | "C:\Program Files (x86)\Mozilla Firefox\firefox" https://localhost:9443
+python cmsimde/wsgi.py 1> nul | "C:\Program Files (x86)\Google\Chrome\Application\chrome" https://localhost:9443
